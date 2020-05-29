cac6dd5f63ab1e8989b9d68c46688fd533daa0c8
chaarvikk <64976954+chaarvikk@users.noreply.github.com>
2020-05-29 11:41:01 +0530

    I commited java servlets

---
+++ GenericServlets/src/EdurekaGeneric/Generic.java
@@ -0,0 +1,31 @@
+package EdurekaGeneric;
+
+import java.io.*;
+import javax.servlet.*;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Generic extends HttpServlet
+{
+	public void service(ServletRequest req, ServletResponse res)
+			throws IOException, ServletException
+	{
+		res.setContentType("text/html") ;
+		
+		PrintWriter pwritter = res.getWriter();
+		pwritter.print("<html>");
+		pwritter.print("<body>");
+		pwritter.print("<h2> Generic Servlet example </h2>");
+		pwritter.print("<p>Welcome to my youtbe channel</p>");
+		pwritter.print("</body>");
+		pwritter.print("</html>");
+		
+	}
+	
+}
+   
