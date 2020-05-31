a4eb2ae41e0e6480d448ff0b523b280b8d8a6e8e
chaarvikk <64976954+chaarvikk@users.noreply.github.com>
2020-05-31 14:30:21 +0530

    commit message

--- GenericServlets/src/EdurekaGeneric/Generic.java
+++ GenericServlets/src/EdurekaGeneric/Generic.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.GenericServlet;
+
 
 
 public class Generic extends HttpServlet
