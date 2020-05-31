26431658f6eafd2505b1c9936392ac2dd7dd5b96
chaarvikk <64976954+chaarvikk@users.noreply.github.com>
2020-05-31 14:38:49 +0530

    Update Addition.java

--- MyFirstCalculator/src/Addition.java
+++ MyFirstCalculator/src/Addition.java
@@ -1,4 +1,38 @@
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Addition {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Addition
+{
+	protected void doPost (HttpServletRequest request , HttpServletResponse response) 
+			throws ServletException, IOException
+	{
+		{
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		
+		String user=request.getParameter("UserName");
+		String pass=request.getParameter("UserPassword");
+		
+		if(user.equals("edueka") && pass.equals("edureka"))
+			pw.println("Login Success...!");
+		else
+			pw.println("Login Failed...!");
+		
+		pw.close();
+		}
+		
+	}
+	
 }
